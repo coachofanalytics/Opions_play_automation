a46d0becf76df254e00ad2e60f4288fdb1dbb3a1
CHRISTOPHER MAGHAS <coachofanalytics@gmail.com>
2023-09-11 02:05:27 -0500

    modify functions

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import subprocess
 import pandas as pd
 import datetime
 import psycopg2
-from utils import merged_data,oversold_overbought
+from utils import merged_data
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -30,23 +30,31 @@ except KeyError:
     #logger.info("Token not available!")
     #raise
 
-user_name= os.environ['USER']
-password= os.environ['PASSWORD']
-host= os.environ['HOST']
+host = 'localhost'
+database_name = "CODA_DEV" #os.environ.get('POSTGRES_DB_NAME') 
+user_name = os.environ.get('POSTGRESDB_USER')
+password = os.environ.get('POSTGRESSPASS') 
 port='5432'
-database_name = os.environ['DATABASE']
 
-USERNAME = os.environ['USERNAME']
-KEY = os.environ['KEY']
+# user_name= os.environ['USER']
+# password= os.environ['PASSWORD']
+# host= os.environ['HOST']
+# port='5432'
+# database_name = os.environ['DATABASE']
 
-def dump_data(df, choice):
+# USERNAME = os.environ['USERNAME']
+# KEY = os.environ['KEY']
+
+def dump_data(choice='other'):
     '''Creating Pipeline for Database'''
     connection_string = f'postgresql://{user_name}:{password}@{host}:{port}/{database_name}'
     engine = create_engine(connection_string)
     Session = sessionmaker(bind=engine)
 
-    osb_df = oversold_overbought()
+    # osb_df = oversold_overbought()
     vl_merged_df = merged_data()
+    new_columns = [x.replace(" ", "_").replace("/", "_").lower() for x in vl_merged_df.columns]
+    print("vl_merged_df======>",new_columns)
     if choice == 'CreditSpreadFile':
         df = pd.read_csv('credit_spread.csv')
         new_columns = [x.replace(" ", "_").replace("/", "_").lower() for x in df.columns]
@@ -67,25 +75,13 @@ def dump_data(df, choice):
         df['is_active'] = True
         df['is_featured'] = True
 
-        columns_to_keep_from_df=['symbol','strategy','type','price','sell_strike','buy_strike','expiry','premium','width','prem_width','rank','earnings_date','comment','on_date','is_active','is_featured']
-        # Select the columns you want to keep from 'df'
-        df_selected_columns = df[columns_to_keep_from_df]
-        try:
-            merged_df = pd.merge(df_selected_columns, vl_merged_df[['symbol']], on='symbol', how='inner')
-        except:
-            print('NO DATA')
+        merged_df = pd.merge(df, vl_merged_df[['symbol']], on='symbol', how='inner')
             
-        # Merge 'df_selected_columns' with 'osb_df' using the selected columns
-        fm_df = pd.merge(merged_df[columns_to_keep_from_df], osb_df[['symbol']], on='symbol', how='inner')
-
         # Generate unique IDs for the new data in merged_df
-        fm_df['id'] = range(1, len(fm_df) + 1)
-
-        # Generate unique IDs for the new data in merged_df
-        fm_df['id'] = range(1, len(fm_df) + 1)
+        merged_df['id'] = range(1, len(merged_df) + 1)
 
         # Replace old records with new data in the database table
-        fm_df.to_sql('investing_credit_spread', engine, if_exists='replace', index=False)
+        merged_df.to_sql('investing_credit_spread', engine, if_exists='replace', index=False)
         
     elif choice == 'coveredCalls':
         df = pd.read_csv('covered_calls.csv')
@@ -108,24 +104,13 @@ def dump_data(df, choice):
         df['is_active'] = True
         df['is_featured'] = True
 
-        columns_to_keep_from_df=['symbol','action','expiry','days_to_expiry','strike_price','mid_price','bid_price','ask_price','rank','earnings_date','earnings_flag','stock_price','raw_return','annualized_return','distance_to_strike','comment','on_date','is_active','is_featured']
-        
-        # Select the columns you want to keep from 'df'
-        df_selected_columns = df[columns_to_keep_from_df]
-        try:
-            merged_df = pd.merge(df_selected_columns, vl_merged_df[['symbol']], on='symbol', how='inner')
-        except:
-            print('NO DATA')
+        merged_df = pd.merge(df, vl_merged_df[['symbol']], on='symbol', how='inner')
             
-        # Merge 'df_selected_columns' with 'osb_df' using the selected columns
-        fm_df = pd.merge(merged_df[columns_to_keep_from_df], osb_df[['symbol']], on='symbol', how='inner')
-
         # Generate unique IDs for the new data in merged_df
-        fm_df['id'] = range(1, len(fm_df) + 1)
-
-        # Replace old records with new data in the database table
-        fm_df.to_sql('investing_covered_calls', engine, if_exists='replace', index=False)
+        merged_df['id'] = range(1, len(merged_df) + 1)
         
+        # Replace old records with new data in the database table
+        merged_df.to_sql('investing_credit_spread', engine, if_exists='replace', index=False)
         
     else:
         df = pd.read_csv('shortput.csv')
@@ -133,6 +118,7 @@ def dump_data(df, choice):
         df.columns = new_columns
         # df['id'] = df.reset_index().index
         df['implied_volatility_rank'] = df['implied_volatility_rank'].str.replace('%', '').astype('float')
+        df.rename(columns={'implied_volatility_rank': 'rank'}, inplace=True)
         df['raw_return'] = df['raw_return'].str.replace('%', '').astype('float')
         df['annualized_return'] = df['annualized_return'].str.replace('%', '').str.replace('∞', '0').astype('float')
         df['stock_price'] = df['stock_price'].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype(float)
@@ -147,123 +133,112 @@ def dump_data(df, choice):
         df['is_active'] = True
         df['is_featured'] = True
 
-        columns_to_keep_from_df=['symbol','action','expiry','days_to_expiry','strike_price','mid_price','bid_price','ask_price','rank','earnings_date','earnings_flag','stock_price','raw_return','annualized_return','distance_to_strike','comment','on_date','is_active','is_featured']
-        
-        # Select the columns you want to keep from 'df'
-        df_selected_columns = df[columns_to_keep_from_df]
-        try:
-            merged_df = pd.merge(df_selected_columns, vl_merged_df[['symbol']], on='symbol', how='inner')
-        except:
-            print('NO DATA')
+        merged_df = pd.merge(df, vl_merged_df[['symbol']], on='symbol', how='inner')
             
-        # Merge 'df_selected_columns' with 'osb_df' using the selected columns
-        fm_df = pd.merge(merged_df[columns_to_keep_from_df], osb_df[['symbol']], on='symbol', how='inner')
-
         # Generate unique IDs for the new data in merged_df
-        fm_df['id'] = range(1, len(fm_df) + 1)
-
+        merged_df['id'] = range(1, len(merged_df) + 1)
+        
         # Replace old records with new data in the database table
-        df.to_sql('investing_shortput', engine, if_exists='replace', index=False)
+        merged_df.to_sql('investing_credit_spread', engine, if_exists='replace', index=False)
         
-def parse_data(html, choice):
-    '''Extract the data table'''
-    result = subprocess.run(["playwright", "install"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# def parse_data(html, choice):
+#     '''Extract the data table'''
+#     result = subprocess.run(["playwright", "install"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    soup = BeautifulSoup(html, 'html.parser')
-    table = soup.select(f'table#{choice}')[0]
-    columns = table.find('thead').find_all('th')
-    df = pd.read_html(str(table))[0]
-    df = df.iloc[:-1,:]
+#     soup = BeautifulSoup(html, 'html.parser')
+#     table = soup.select(f'table#{choice}')[0]
+#     columns = table.find('thead').find_all('th')
+#     df = pd.read_html(str(table))[0]
+#     df = df.iloc[:-1,:]
     
-    for col in df.columns:
-        if df[col].dtype == object:
-            df[col] = df[col].str[:255]
-    return df
+#     for col in df.columns:
+#         if df[col].dtype == object:
+#             df[col] = df[col].str[:255]
+#     return df
     
-def extract_data(url, choice):
-    '''Extract the HTML code'''
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True, slow_mo=50, chromium_sandbox = False)
-        page = browser.new_page()
+# def extract_data(url, choice):
+#     '''Extract the HTML code'''
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=True, slow_mo=50, chromium_sandbox = False)
+#         page = browser.new_page()
         
-        page.goto(url=url)
+#         page.goto(url=url)
 
-        page.fill(
-           'input#Login', USERNAME
-        )
-        page.fill(
-            'input#Password', KEY
-        )
+#         page.fill(
+#            'input#Login', USERNAME
+#         )
+#         page.fill(
+#             'input#Password', KEY
+#         )
 
-        #click on submit button
-        page.click("button[type=submit]")
+#         #click on submit button
+#         page.click("button[type=submit]")
 
-        # Wait to load the HTML template
-        try:
-            page.wait_for_timeout(30000)
-        except:
-            page.wait_for_timeout(60000)
+#         # Wait to load the HTML template
+#         try:
+#             page.wait_for_timeout(30000)
+#         except:
+#             page.wait_for_timeout(60000)
 
-        if choice == 'CreditSpreadFile':
-            html = page.inner_html('//*[@id="CreditSpreadFile_wrapper"]')
+#         if choice == 'CreditSpreadFile':
+#             html = page.inner_html('//*[@id="CreditSpreadFile_wrapper"]')
 
-        elif choice == 'coveredCalls':
-            html = page.inner_html('//*[@id="coveredCalls_wrapper"]')
+#         elif choice == 'coveredCalls':
+#             html = page.inner_html('//*[@id="coveredCalls_wrapper"]')
 
-        else:
-            html = page.inner_html('//*[@id="shortPuts_wrapper"]')
-        return html
+#         else:
+#             html = page.inner_html('//*[@id="shortPuts_wrapper"]')
+#         return html
 
-#Executing the options play tables
-def main_covered_calls():
-    # print("COVERED CALLS")
-    try:
-        urls = {
-            'coveredCalls' : 'https://www.optionsplay.com/hub/covered-calls'
-        }
+# #Executing the options play tables
+# def main_covered_calls():
+#     # print("COVERED CALLS")
+#     try:
+#         urls = {
+#             'coveredCalls' : 'https://www.optionsplay.com/hub/covered-calls'
+#         }
 
-        html = extract_data(url=urls['coveredCalls'], choice='coveredCalls')
-        data = parse_data(html, choice='coveredCalls')
-        data.to_csv('covered_calls.csv', index=False)
-        dump_data(df=data, choice='coveredCalls')
-    except Exception as err:
-        print(err)
+#         html = extract_data(url=urls['coveredCalls'], choice='coveredCalls')
+#         data = parse_data(html, choice='coveredCalls')
+#         data.to_csv('covered_calls.csv', index=False)
+#         dump_data(df=data, choice='coveredCalls')
+#     except Exception as err:
+#         print(err)
     
-    
+# def main_shortput():
+#     print("SHORTPUT")
+#     try:
+#         urls = {
+#             'shortPuts' : 'https://www.optionsplay.com/hub/short-puts'
+#         }
 
-def main_shortput():
-    print("SHORTPUT")
-    try:
-        urls = {
-            'shortPuts' : 'https://www.optionsplay.com/hub/short-puts'
-        }
+#         html = extract_data(url=urls['shortPuts'], choice='shortPuts')
+#         data = parse_data(html, choice='shortPuts')
+#         data.to_csv('shortput.csv', index=False)
+#         dump_data(df=data, choice='shortPuts')
+#     except Exception as err:
+#         print(err)
 
-        html = extract_data(url=urls['shortPuts'], choice='shortPuts')
-        data = parse_data(html, choice='shortPuts')
-        data.to_csv('shortput.csv', index=False)
-        dump_data(df=data, choice='shortPuts')
-    except Exception as err:
-        print(err)
+# def main_cread_spread():
+#     print("CREDITSPREAD")
+#     try:
+#         urls = {
+#             'CreditSpreadFile' : 'https://www.optionsplay.com/hub/credit-spread-file'
+#         }
 
-def main_cread_spread():
-    print("CREDITSPREAD")
-    try:
-        urls = {
-            'CreditSpreadFile' : 'https://www.optionsplay.com/hub/credit-spread-file'
-        }
-
-        html = extract_data(url=urls['CreditSpreadFile'], choice='CreditSpreadFile')
-        data = parse_data(html, choice='CreditSpreadFile')  
-        data.to_csv('credit_spread.csv', index=False)
-        dump_data(df=data, choice='CreditSpreadFile') 
-    except Exception as err:
-        print(err)
+#         html = extract_data(url=urls['CreditSpreadFile'], choice='CreditSpreadFile')
+#         data = parse_data(html, choice='CreditSpreadFile')  
+#         data.to_csv('credit_spread.csv', index=False)
+#         dump_data(df=data, choice='CreditSpreadFile') 
+#     except Exception as err:
+#         print(err)
 
 
 if __name__ == '__main__':
-    main_cread_spread()
-    main_shortput()
-    main_covered_calls()
+    dump_data()
+    # main_cread_spread()
+    # main_shortput()
+    # main_covered_calls()
     # send_email()
     time = datetime.datetime.now()
     logger.info(f'Code Executed : {time}')
--- utils.py
+++ utils.py
@@ -12,15 +12,20 @@ except KeyError:
     #logger.info("Token not available!")
     #raise
 
-user_name= os.environ['USER']
-password= os.environ['PASSWORD']
-host= os.environ['HOST']
+# user_name= os.environ['USER']
+# password= os.environ['PASSWORD']
+# host= os.environ['HOST']
+# port='5432'
+# database_name = os.environ['DATABASE']
+
+# USERNAME = os.environ['USERNAME']
+# KEY = os.environ['KEY']
+
+host = 'localhost'
+database_name = "CODA_DEV" #os.environ.get('POSTGRES_DB_NAME') 
+user_name = os.environ.get('POSTGRESDB_USER')
+password = os.environ.get('POSTGRESSPASS') 
 port='5432'
-database_name = os.environ['DATABASE']
-
-USERNAME = os.environ['USERNAME']
-KEY = os.environ['KEY']
-
 
 DB_CONFIG = {
     'dbname': database_name,
@@ -29,7 +34,6 @@ DB_CONFIG = {
     'host': host,
     'port': port
 }
-
 
 
 def get_db_connection():
@@ -50,6 +54,35 @@ def load_data_from_db(ticker_symbol):
     except Exception as e:
         print(f"Error fetching data from DB for {ticker_symbol}: {e}")
         return None
+    
+def load_oversolddata_from_db():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM investing_oversold", 
+                       (datetime.now() - timedelta(days=15)))
+        data = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+        return data
+    except Exception as e:
+        print(f"Error fetching data failed: {e}")
+        return None
+
+
+def read_data_from_csv(csv_file_path):
+    #getting liquidity
+    try:
+        # osb_df= pd.read_csv('oversold_overbought_09102023.csv')
+        csv_df= pd.read_csv(csv_file_path)
+    except Exception as e:
+        print(f"Error reading data from CSV file: {e}")
+        return []
+    csv_df_dict = csv_df.to_dict(orient='records')
+    return csv_df,csv_df_dict
+
 
 def save_data_to_db(ticker_symbol, data):
     try:
@@ -84,6 +117,38 @@ def save_data_to_db(ticker_symbol, data):
     except Exception as e:
         print(f"Error saving data to DB for {ticker_symbol}: {e}")
 
+
+def save_oversold_data_to_db(data):
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        # Insert data from CSV file into the Oversold table
+        csv_file_path = 'oversold_overbought_09102023.csv'
+        oversold_data = read_data_from_csv(csv_file_path)[1]
+        print(oversold_data)
+        if oversold_data:
+            insert_oversold_sql = """
+                INSERT INTO investing_oversold
+                (symbol, description, condition)
+                VALUES (%s, %s, %s);
+            """
+            cursor.executemany(
+                insert_oversold_sql,
+                [(item['symbol'], item['description'], item['condition'])
+                for item in oversold_data]
+            )
+            conn.commit()
+            print("Data inserted successfully.")
+        else:
+            print("No valid data found in the CSV file.")
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(f"Error saving oversold data: {e}")
+
+
 def fetch_data_util(ticker_symbol):
     db_data = load_data_from_db(ticker_symbol)
     
@@ -97,56 +162,74 @@ def fetch_data_util(ticker_symbol):
             valuation_keys = ["overallRisk", "sharesShort", "enterpriseToEbitda", "ebitda", "quickRatio", "currentRatio", "revenueGrowth"]
             data = {key: full_data.get(key, None) for key in valuation_keys}
             
-            save_data_to_db(ticker_symbol, data)
+            save_data_to_db(data,ticker_symbol)
         except Exception as e:
             print(f"Error fetching data from yfinance for {ticker_symbol}: {e}")
             return {}
     return data
 
-def unusual_volume():
-    # path='C:/Users/CHRIS/web/opa/app/data/unusual_volume.csv'
-    # udf = pd.read_csv(path, encoding='utf-8') if os.path.exists(path) else print("File does not exist")
-    udf= pd.read_csv('unusual_volume.csv')
-    new_columns = [x.replace(" ", "_").replace("/", "_").lower() for x in udf.columns]
-    udf.columns = new_columns
-    udf['price'] = udf['price'].astype(float)
-    udf['volume'] = udf['volume'].astype(float)
-    filtered_udf = udf[(udf['volume'] > 1000) & (udf['price'] >= 15)  & (udf['price'] <= 150)]
-    return filtered_udf
+def fetch_oversold_util():
+    db_data = load_oversolddata_from_db()
+    
+    if db_data:
+        columns = ["symbol"]
+        data = dict(zip(columns, db_data))
+    else:
+        try:
+            csv_file_path_osb='oversold_overbought_09102023.csv'
+            data= read_data_from_csv(csv_file_path_osb)[0]
+            save_oversold_data_to_db(data)
 
-def liquidity():
-    #getting liquidity
-    ldf= pd.read_csv('liquidity.csv')
-    new_columns = [x.replace(" ", "_").replace("/", "_").lower() for x in ldf.columns]
-    ldf.columns = new_columns
-    ldf['liquidity'] = ldf['liquidity'].astype(float)
-    # converstion and cleanup
-    ldf['liquidity'] = ldf['liquidity'].astype(float)
-    # filtered_df = df[(df['rank'] > 15) & (df['rank'] <= 75) & (df['price'] >= 15)]
-    filtered_ldf = ldf[(ldf['liquidity'] == 1)]
-    return filtered_ldf
-
-def oversold_overbought():
-    #getting liquidity
-    osb_df= pd.read_csv('oversold_overbought_09102023.csv')
-    new_columns = [x.replace(" ", "_").replace("/", "_").lower() for x in osb_df.columns]
-    osb_df.columns = new_columns
-    return osb_df
+        except Exception as e:
+            print(f"Error fetching data from yfinance : {e}")
+            return {}
+    return data
 
 
 def merged_data():
     # Merge liquidity and unusual volume
-    unusual_df = unusual_volume()
-    liquidity_df = liquidity()
+    db_data = fetch_oversold_util()
+
+    csv_file_path_uv='unusual_volume.csv'
+    csv_file_path_lq='liquidity.csv'
+    csv_file_path_osb='oversold_overbought_09102023.csv'
+    unusual_df = read_data_from_csv(csv_file_path_uv)[0]
+    liquidity_df = read_data_from_csv(csv_file_path_lq)[0]
+    osb_df = read_data_from_csv(csv_file_path_osb)[0]
+    # osb_df_v2 = db_data
+
     vl_merged_df = pd.merge(unusual_df,liquidity_df [['symbol']], on='symbol', how='inner')
-    vl_merged_df_columns = [x.replace(" ", "_").replace("/", "_").lower() for x in vl_merged_df.columns]
+
+    merged_df = pd.merge(vl_merged_df, osb_df[['symbol']], on='symbol', how='inner')
 
     # Fetch EBITDA for each symbol in unusual_df
-    vl_merged_df['ebitda'] = vl_merged_df['symbol'].apply(lambda x: fetch_data_util(x).get('ebitda', None))
+    merged_df['ebitda'] = merged_df['symbol'].apply(lambda x: fetch_data_util(x).get('ebitda', None))
 
     # Filter out symbols where EBITDA <= 0
-    positive_ebitda_df = vl_merged_df[vl_merged_df['ebitda'] > 0]
+    positive_ebitda_df = merged_df[merged_df['ebitda'] > 0]
 
     # Apply other filters
-    filtered_df = positive_ebitda_df[(positive_ebitda_df['price'] >= 15) & (positive_ebitda_df['volume'] > 1000)]
+    # filtered_df = positive_ebitda_df[(positive_ebitda_df['price'] >= 15) & (positive_ebitda_df['volume'] > 1000)]
+    filtered_df = positive_ebitda_df
+
+        # Print total records from both files
+    # print(f"Total records in unusualvolume.csv: {len(unusual_df)}")
+    # print(f"Total records in liquidity.csv: {len(liquidity_df)}")
+    print(f"Total records in osb.csv: {len(osb_df)}")
+    # # print("vl_merged_df_columns====>",vl_merged_df_columns)
+    # print(f"Total records in vl_merged_df.csv: {len(vl_merged_df)}")
+    # print("merged_df=====>",merged_df_columns)
+    # print(f"Total records in merged_df: {len(merged_df)}")
+    # print(f"Total records in positive_ebitda_df: {len(positive_ebitda_df)}")
+    # print(f"Total records in filtered_df with +ve ebitda: {len(filtered_df)}")
     return filtered_df
+
+
+if __name__ == '__main__':
+    merged_data()
+    # main_cread_spread()
+    # main_shortput()
+    # main_covered_calls()
+    # send_email()
+    # time = datetime.datetime.now()
+    # logger.info(f'Code Executed : {time}')
